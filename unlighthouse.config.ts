85eaf8227a8a04bf4efc67cdb73238bdbbd93c85
Tiago Montanha <tcarvalhovb@gmail.com>
2025-07-09 09:44:15 -0300

    fix(lighthouse): set fixed port to avoid conflict with api

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -2,6 +2,9 @@ export default {
   // URL base do site em ambiente de preview (produção)
   site: 'http://localhost:4173',
 
+  // Define uma porta fixa para a UI do Unlighthouse para evitar conflitos
+  port: 5678,
+
   scanner: {
     // Não é necessário especificar o diretório `dist` se estamos auditando um servidor em execução
     exclude: ['/404'],
