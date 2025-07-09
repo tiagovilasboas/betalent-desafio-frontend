2f20ee855074edea94e81ac8020bd755f2749314
Tiago Montanha <tcarvalhovb@gmail.com>
2025-07-09 10:13:02 -0300

    refactor(home): revert lazy loading from home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,23 +1,5 @@
-import { lazy, Suspense } from 'react';
-
-import { Center } from '@mantine/core';
-
-import EmployeeTableSkeleton from '@/features/employees/components/EmployeeTableSkeleton';
-
-const LazyEmployees = lazy(
-  () => import('@/features/employees/components/Employees'),
-);
+import Employees from '@/features/employees/components/Employees';
 
 export function Component() {
-  return (
-    <Suspense
-      fallback={
-        <Center style={{ height: '100%' }}>
-          <EmployeeTableSkeleton />
-        </Center>
-      }
-    >
-      <LazyEmployees />
-    </Suspense>
-  );
+  return <Employees />;
 } 
